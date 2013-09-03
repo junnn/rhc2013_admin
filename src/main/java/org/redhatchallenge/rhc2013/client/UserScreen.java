112b18a919ede8f47b467c9b7a61ceba20732e00
Geraldlim91 <gerald_limjj@yahoo.com.sg>
2013-09-03 14:43:17 +0800

    Time Slot get from DB

--- src/main/java/org/redhatchallenge/rhc2013/client/UserScreen.java
+++ src/main/java/org/redhatchallenge/rhc2013/client/UserScreen.java
@@ -99,7 +99,9 @@ public class UserScreen extends Composite {
 
                 initCellTable();
             }
+
         });
+
 
         userService.getListOfTimeSlot(new AsyncCallback<List<TimeSlotList>>() {
             @Override
@@ -114,7 +116,10 @@ public class UserScreen extends Composite {
                     Date date = convertTimeSlot(d.getTimeslot());
                     String formatedDate = returnLongDateTime(DateTimeFormat.getFormat(DateTimeFormat.PredefinedFormat.DATE_TIME_SHORT).format(date));
                     dateList.add(formatedDate);
-
+                }
+                timeSlotList.insertItem("Select All", 0);
+                for(int i = 0; i < dateList.size(); i++){
+                    timeSlotList.insertItem(dateList.get(i).toString(),i+1);
                 }
             }
         });
