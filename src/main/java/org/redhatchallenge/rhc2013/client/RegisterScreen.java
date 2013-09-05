868592895a2be042bd9634f5bb0d0fd1962d0cdf
WinnieT <tanxiangfei@gmail.com>
2013-09-05 12:00:32 +0800

    Fix Ui and Add random genrate password in Reset Password Screen

--- src/main/java/org/redhatchallenge/rhc2013/client/RegisterScreen.java
+++ src/main/java/org/redhatchallenge/rhc2013/client/RegisterScreen.java
@@ -232,7 +232,7 @@ public class RegisterScreen extends Composite {
             @Override
             public void onSuccess(Boolean bool) {
                 if(bool) {
-                    messageLabel.setText("Successful");
+                    messageLabel.setText("Contestant Created Successfulyl");
                     emailField.setText("");
                     passwordField.setText("");
                     confirmPasswordField.setText("");
