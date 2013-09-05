e1fe1265af864634348c7b4448ed44ee9b93fadc
Jun Hao Ng <junhao94ng@gmail.com>
2013-09-05 10:38:05 +0800

    Bug for sending email multiplt times fixed.

--- src/main/java/org/redhatchallenge/rhc2013/client/UserServiceAsync.java
+++ src/main/java/org/redhatchallenge/rhc2013/client/UserServiceAsync.java
@@ -27,7 +27,7 @@ public interface UserServiceAsync {
 
     void exportCsv(List<Student> students, AsyncCallback<String> async);
 
-    void resetPassword(String password, String email, AsyncCallback<Boolean> async);
+    void resetPassword(String password, List<Student> students, AsyncCallback<Boolean> async);
 
     void assignTimeslotAndQuestions(String email, AsyncCallback<Void> async);
 }
