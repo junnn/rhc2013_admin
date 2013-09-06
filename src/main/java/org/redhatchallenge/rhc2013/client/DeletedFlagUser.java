d6aaf117ef9e43d5d4f68e866163711ff0d69dfd
Jun Hao Ng <junhao94ng@gmail.com>
2013-09-06 10:21:48 +0800

    minorchanges

---
+++ src/main/java/org/redhatchallenge/rhc2013/client/DeletedFlagUser.java
@@ -0,0 +1,11 @@
+package org.redhatchallenge.rhc2013.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jun
+ * Date: 6/9/13
+ * Time: 10:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DeletedFlagUser {
+}
