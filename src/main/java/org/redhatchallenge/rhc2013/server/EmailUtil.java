15a485a4840ce0342040df58924605c29da10f33
Jun Hao Ng <junhao94ng@gmail.com>
2013-09-19 15:41:03 +0800

    Cleaned up SMTP authentication credentials.

---
+++ src/main/java/org/redhatchallenge/rhc2013/server/EmailUtil.java
@@ -0,0 +1,37 @@
+package org.redhatchallenge.rhc2013.server;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.mail.EmailConstants;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+
+public class EmailUtil {
+    private static HtmlEmail mail;
+
+    public static void sendEmail(String subject, String htmlMessage, String textMessage, final String recipient) {
+
+        mail = new HtmlEmail();
+
+        try {
+            mail.setHostName("smtp.mailgun.org");
+            mail.setSmtpPort(465);
+            mail.setSSLOnConnect(true);
+            mail.setAuthentication("postmaster@redhatchallenge.mailgun.org", "");
+            mail.setCharset(EmailConstants.UTF_8);
+            mail.setFrom("contact@redhatchallenge.mailgun.org");
+            mail.setSubject(subject);
+            mail.setTextMsg(textMessage);
+            mail.setHtmlMsg(htmlMessage);
+            mail.addTo(recipient);
+
+            mail.send();
+
+        } catch (EmailException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+
+    public static String generateToken(int length) {
+        return RandomStringUtils.randomAlphanumeric(length);
+    }
+}
